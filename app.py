5f70894321f6279e5a18b2c21e6e0e22df1c270a
Mahdi-Ettehadnejad <mahdi.ettehad85@gmail.com>
2022-09-19 09:30:18 +0430

    data changed (commit 2)

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from sklearn.impute import SimpleImputer
+import seaborn as sns
+import plotly.express as px
+
 
 
 df = pd.read_csv('owid-covid-data.csv')
 
+print(df)
+print(df.shape)
 
-print(df.describe())
+
+# df.dropna(axis=0,inplace=True)
+
+# print(df.head(1))
+# print(df.shape)
+
+
