d39643b31c816e065128a0dd02e0624ea02eb129
Mahdi-Ettehadnejad <mahdi.ettehad85@gmail.com>
2022-09-19 15:00:59 +0430

    Stay here for now i will learn pandas and matplotlib and i'll be back

--- app.py
+++ app.py
@@ -12,3 +12,11 @@ df.drop(['FIPS','Admin2','Province_State','Last_Update','Combined_Key'],axis=1,i
 df.rename(columns={'Country_Region': 'Country'},inplace=True)
 world_grouped_by =  df.groupby('Country')['Confirmed','Active','Recovered','Deaths'].sum().reset_index()
 
+
+# find top 20 countries with maximum number of confirmed cases 
+
+top_20 = world_grouped_by.sort_values(by=['Confirmed'], ascending=False).head(20)
+
+plt.figure(figsize=(2,2))
+
+plot = sns.barplot(top_20['Confirmed'], top_20['Country'])
